50ee4c615725bb2c374002b414eb9dd0bfee3260
ASR0RB3K <asrorbek.design@gmail.com>
2022-01-19 17:50:12 +0500

    Cources Entity

---
+++ Entities/Cources.cs
@@ -0,0 +1,18 @@
+namespace bot.Entities;
+
+public class Cources
+{
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string Tag { get; set; }
+
+    public string Score { get; set; }
+
+    public string CostInUzs { get; set; }
+
+    public int Duration { get; set; }
+    
+    public string BannerId { get; set; } 
+}
