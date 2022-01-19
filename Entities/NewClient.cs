d5e64c7258ce79065e995f59aa835e498cf19286
ASR0RB3K <asrorbek.design@gmail.com>
2022-01-19 17:52:26 +0500

    NewClient Entity

---
+++ Entities/NewClient.cs
@@ -0,0 +1,12 @@
+namespace bot.Entities;
+
+public class NewClient
+{
+    public string UserName { get; set; }
+
+    public string FullName { get; set; }
+
+    public string Phone { get; set; }
+
+    public string InterestedCources { get; set; }
+}
